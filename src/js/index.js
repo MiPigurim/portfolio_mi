b334395a825bd6cd5734e181b31fedb614887ddc
MiPigurim <michelepigurim26@gmail.com>
2024-04-07 16:56:22 -0300

    feat: add script button "Mostrar mais"

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+/* 
+- Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html 
+  Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele 
+  Passo 2 - identificar o clique no botão 
+  Passo 3 - adicionar a classe "ativo" nos projetos escondidos 
+
+- Objetivo 2 - esconder o botão de mostrar mais 
+  Passo 1 - pegar o botão e esconder ele 
+*/
+
+// Objetivo 1
+  // Passo 1
+const btnMostrarMais = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+btnMostrarMais.addEventListener('click', () => {
+  // Passo 3
+  projetosInativos.forEach(projetoInativo => {
+    projetoInativo.classList.add('ativo');
+  })
+
+// Objetivo 2
+  // Passo 1
+  btnMostrarMais.classList.add('remover');
+})
